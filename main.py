97c3258628f4d73e7947f2275abcbf8048c88f4c
Darkshades <henrychibueze774@gmail.com>
2022-09-03 13:49:32 +0100

    main and requiremnt update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ts_model import ARMA
 from pipeline import PipeLine
 from api_utils import TwelveDataApiUtils
+from sklearn.linear_model import LinearRegression
 
 if __name__ == '__main__':
     stock_symbol = 'MSFT'
@@ -9,6 +10,7 @@ if __name__ == '__main__':
     active_duration = 3600 #1 hour
 
     api_utils = TwelveDataApiUtils(stock_symbol, interval, n_samples)
-    arma_model = ARMA((2, 1), estimator_lags=3)
+    regressor = LinearRegression()
+    arma_model = ARMA((2, 1), base_regressor=regressor, estimator_lags=3)
     pipeline = PipeLine(arma_model, api_utils, session_duration=active_duration)
     pipeline.eventLoop()
